c49673f3300624e4e251749f0964aafc24abfe58
abdelaziz mohamed <abdelazizmohamed1233@gmail.com>
2021-02-07 08:03:08 +0200

    Initial commit

---
+++ first.js
@@ -0,0 +1,15 @@
+console.log("welcome everybody");
+
+
+console.log("welcome everybody");
+
+
+var _welcome_everybody = "welcome everybody" , my_name=15;
+
+
+console.time('t');
+
+console.log(my_name+"hello everybody");
+
+console.timeEnd('t');
+
